19180b10e227e5838f378fe6205ce9767c41a87a
Victoria0721 <80014979+Victoria0721@users.noreply.github.com>
2021-03-11 12:51:43 +0800

    Add files via upload

---
+++ HW01_0304.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  6 17:50:24 2021
+
+@author: Victoria
+"""
+
+sum= 0
+for i in range(0,51,2):
+    sum = sum + i
+print("1~50偶數總合:") 
+print (sum) 
+
+
+
+
+sum = 0
+for i in range(1,51,2):
+    sum = sum + i
+print("1~50奇數總合:") 
+print(sum) 
---
+++ HW02_0304.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  6 17:23:53 2021
+
+@author: Victoria
+"""
+
+for i in range(1,10):
+    if i % 2 == 0:
+        print((i),end=",")
+    
+    
